1cf7d16cb89b588499367ef6c4984bf20039b54a
10542hsrif <81347603+10542hsrif@users.noreply.github.com>
2024-05-22 20:28:11 +0800

    Update nvidia.py

--- nvidia.py
+++ nvidia.py
@@ -8,7 +8,7 @@ if driver in ["y", "Y", "yes", "Yes"]:
     os.system('sh apt-sid.sh')
     os.system('systemctl reboot')
 elif driver in ["n", "N", "no", "No"]:
-    exit()
+    os.system('systemctl reboot')
 else:
     os.system('sh apt-sid.sh')
     os.system('systemctl reboot')
