284b77a21ebbee6268cd6a4943154c073ee00184
10542hsrif <81347603+10542hsrif@users.noreply.github.com>
2024-05-22 12:40:53 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
-sudo apt install -y vim lightdm i3 i3lock lxappearance feh qutebrowser ranger gnome-disk-utility polybar zathura alacritty pavucontrol bluez mpv neofetch rofi conky brave python3 python3-pip git scrot ytfzf vlc xorg lightdm lightdm-slick-greeter
+sudo apt install -y vim lightdm i3 i3lock lxappearance feh qutebrowser ranger gnome-disk-utility polybar zathura xfce4-terminal pavucontrol bluez mpv neofetch rofi conky brave python3 python3-pip git scrot ytfzf vlc xorg lightdm lightdm-slick-greeter guvcview lightdm-settings xrand arandr
 tar -xvf powerline-shell.tar.gz ; cd powerline-shell ; python3 setup.py install
 cd ..
 git clone https://gitlab.com/10542hsrif/gruvbox-dark-gtk ~/.themes/gruvbox-dark-gtk
@@ -13,5 +13,6 @@ cp bashrc .bashrc ; rm bashrc ; mv .bashrc $HOME
 mv conky/ ~/.config/
 mv i3/ ~/.config/
 sudo mv lightdm.conf /etc/lightdm/
+sudo systemctl enable lightdm
 clear
 echo "Installation complete, you may start configuring the system"
