7f384dca07fba7fde98b4147e41db46cc7f802c8
10542hsrif <81347603+10542hsrif@users.noreply.github.com>
2024-05-22 18:22:24 +0800

    Update apt-sid.sh

--- apt-sid.sh
+++ apt-sid.sh
@@ -1,4 +1,7 @@
 sudo mv sid-repositories.list /etc/apt/sources.list.d/
-sudo apt update && sudo apt install nvidia-legacy-340xx-driver firmware-misc-nonfree
+sudo apt update && sudo apt install -y nvidia-legacy-340xx-driver firmware-misc-nonfree
 sudo rm /etc/apt/sources.list.d/sid-repositories.list
 sudo apt update
+clear
+echo "Installation complete, you may now start your system."
+sleep 3
