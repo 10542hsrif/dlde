562cf8a366f306c53b5bffb6bd1499668f55b06e
10542hsrif <81347603+10542hsrif@users.noreply.github.com>
2024-05-22 19:04:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt install -y vim lightdm i3 i3lock lxappearance feh qutebrowser ranger gnome-disk-utility polybar zathura xfce4-terminal pavucontrol bluez mpv neofetch rofi conky python3 python3-pip git scrot ytfzf vlc xorg lightdm slick-greeter guvcview lightdm-settings arandr btop libreoffice chrony
 tar -xvf powerline-shell.tar.gz ; cd powerline-shell ; sudo python3 setup.py install
-cd ..
+cd $HOME/dlde/
 git clone https://gitlab.com/10542hsrif/gruvbox-dark-gtk ~/.themes/gruvbox-dark-gtk
 tar -xvf gruvbox_dark.tar.gz ; mv gruvbox_dark/ ~/.icons/gruvbox_dark/
 tar -xvf solarized.tar.gz ; mv solarized ~/.themes/solarized/
